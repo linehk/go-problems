d7d7fe1ba280a40a066f7e9ce9f9894f982bcacb
sulinehk <sulinehk@gmail.com>
2019-01-09 21:50:13 +0800

    easy: add array

---
+++ easy/array/1.go
@@ -0,0 +1,13 @@
+package array
+
+func removeDuplicates(nums []int) int {
+        pmap := make(map[int]int)
+        for _, v := range nums {
+                if _, ok := pmap[v]; !ok {
+                        pmap[v] = len(pmap)
+                        nums[pmap[v]] = v
+                }
+        }
+        return len(pmap)
+}
+
