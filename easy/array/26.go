7b9d25f050782e8b4109e795dc3803930cc4cefb
sulinehk <sulinehk@gmail.com>
2019-01-12 20:22:17 +0800

    easy/array: fix 26 values

--- easy/array/26.go
+++ easy/array/26.go
@@ -1,12 +1,12 @@
 package array
 
 func removeDuplicates(nums []int) int {
-        pmap := make(map[int]int)
+        m := make(map[int]int)
         for _, v := range nums {
-                if _, ok := pmap[v]; !ok {
-                        pmap[v] = len(pmap)
-                        nums[pmap[v]] = v
+                if _, ok := m[v]; !ok {
+                        m[v] = len(m)
+                        nums[m[v]] = v
                 }
         }
-        return len(pmap)
+        return len(m)
 }
