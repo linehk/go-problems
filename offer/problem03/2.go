858b83414a33d137268e5449c2fd887d541f5f1a
sulinehk <sulinehk@gmail.com>
2019-01-22 22:39:52 +0800

    offer/problem03: add 2.go

--- offer/problem03/2.go
+++ offer/problem03/2.go
@@ -1,9 +1,65 @@
 package problem03
 
-// 计数法
+// 计数法（不可得全部）
+// O(n) O(n)
 func duplicate5(nums []int) (int, bool) {
 	s := make([]int, len(nums))
+	for _, v := range nums {
+		if s[v] == v {
+			return v, true
+		} else {
+			s[v] = v
+		}
+	}
+	return 0, false
 }
 
+// 计数法（两次扫描，可得全部）
+// O(2n) O(n)
 func duplicate6(nums []int) (int, bool) {
+	s := make([]int, len(nums))
+	for _, v := range nums {
+		s[v]++
+	}
+	for _, v := range s {
+		if v > 1 {
+			return v, true
+		}
+	}
+	return 0, false
+}
+
+// 二分法
+// O(nlgn) O(1)
+func duplicate7(nums []int) (int, bool) {
+	start := 1
+	end := len(nums) - 1
+	for start <= end {
+		mid := start + (end-start)/2
+		count := countRange(nums, start, mid)
+		if start == end {
+			if count > 1 {
+				return start, true
+			} else {
+				break
+			}
+		}
+		if count > (mid - start + 1) {
+			end = mid
+		} else {
+			start = mid + 1
+		}
+	}
+	return 0, false
+}
+
+// 统计数组元素在 [start, end] 区间出现过的次数
+func countRange(nums []int, start, end int) int {
+	count := 0
+	for _, v := range nums {
+		if start <= v && v <= end {
+			count++
+		}
+	}
+	return count
 }
--- offer/problem03/2_test.go
+++ offer/problem03/2_test.go
@@ -1,1 +1,58 @@
 package problem03
+
+import (
+	"testing"
+)
+
+func TestDuplicate2(t *testing.T) {
+	tests := []struct {
+		input    []int
+		want     []int
+		wantBool bool
+	}{
+		{[]int{2, 3, 5, 4, 3, 2, 6, 7}, []int{2, 3}, true},
+	}
+	for i, tt := range tests {
+		got1, ok1 := duplicate5(tt.input)
+		if ok1 != tt.wantBool {
+			t.Errorf("%v. got1 %v, want %v", i, ok1, tt.wantBool)
+		}
+		f1 := false
+		for _, v := range tt.want {
+			if got1 == v {
+				f1 = true
+			}
+		}
+		if !f1 {
+			t.Errorf("%v. got1 %v, want %v", i, got1, tt.want)
+		}
+
+		got2, ok2 := duplicate6(tt.input)
+		if ok2 != tt.wantBool {
+			t.Errorf("%v. got2 %v, want %v", i, ok2, tt.wantBool)
+		}
+		f2 := false
+		for _, v := range tt.want {
+			if got2 == v {
+				f2 = true
+			}
+		}
+		if !f2 {
+			t.Errorf("%v. got2 %v, want %v", i, got2, tt.want)
+		}
+
+		got3, ok3 := duplicate7(tt.input)
+		if ok3 != tt.wantBool {
+			t.Errorf("%v. got3 %v, want %v", i, ok3, tt.wantBool)
+		}
+		f3 := false
+		for _, v := range tt.want {
+			if got3 == v {
+				f3 = true
+			}
+		}
+		if !f3 {
+			t.Errorf("%v. got3 %v, want %v", i, got3, tt.want)
+		}
+	}
+}
