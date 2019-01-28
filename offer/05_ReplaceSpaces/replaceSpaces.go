ade81c2007bca07b54f8e50b2057fa9d1dbaab61
sulinehk <sulinehk@gmail.com>
2019-01-28 14:13:36 +0800

    offer/05_ReplaceSpaces: refactor

--- offer/05_ReplaceSpaces/replaceSpaces.go
+++ offer/05_ReplaceSpaces/replaceSpaces.go
@@ -14,19 +14,19 @@ func replaceSpaces(str string) string {
 	}
 	newStr := make([]byte, len(oriStr)+2*count)
 	copy(newStr, oriStr)
-	oriLastIndex := len(oriStr) - 1
-	newLastIndex := len(newStr) - 1
-	for ; oriLastIndex >= 0; oriLastIndex-- {
-		if newStr[oriLastIndex] == ' ' {
-			newStr[newLastIndex] = '0'
-			newLastIndex--
-			newStr[newLastIndex] = '2'
-			newLastIndex--
-			newStr[newLastIndex] = '%'
-			newLastIndex--
+	oriLast := len(oriStr) - 1
+	newLast := len(newStr) - 1
+	for ; oriLast >= 0; oriLast-- {
+		if newStr[oriLast] == ' ' {
+			newStr[newLast] = '0'
+			newLast--
+			newStr[newLast] = '2'
+			newLast--
+			newStr[newLast] = '%'
+			newLast--
 		} else {
-			newStr[newLastIndex] = newStr[oriLastIndex]
-			newLastIndex--
+			newStr[newLast] = newStr[oriLast]
+			newLast--
 		}
 	}
 	return string(newStr)
