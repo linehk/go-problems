d7d9b4844a561c11072562755a89e34252ef7f5e
sulinehk <sulinehk@gmail.com>
2019-02-26 09:57:41 +0800

    leetcode/easy/array: update 0189

--- leetcode/easy/array/0189_rotate_array.go
+++ leetcode/easy/array/0189_rotate_array.go
@@ -5,17 +5,16 @@ func rotate(nums []int, k int) {
 		return
 	}
 	// 取余操作是为了在 k > len(nums) 的情况也能正常运行
+	// 左移 1. 翻转 0 到 k 2. 翻转剩下元素 3. 翻转全部
+	// 右移 先进行第三步
 	k %= len(nums)
-	reverse(nums, 0, len(nums)-1)
-	reverse(nums, 0, k-1)
-	reverse(nums, k, len(nums)-1)
+	reverse(nums)
+	reverse(nums[:k])
+	reverse(nums[k:])
 }
 
-// 翻转从索引 i 到 j 的数
-func reverse(nums []int, i, j int) {
-	for i < j {
+func reverse(nums []int) {
+	for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
 		nums[i], nums[j] = nums[j], nums[i]
-		i++
-		j--
 	}
 }
