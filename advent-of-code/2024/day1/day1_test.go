5ad8951f830116b0a876c9a11a576e6a6f8130e3
sulinehk <sulinehk@gmail.com>
2025-12-28 14:45:11 +0800

    update advent-of-code day1

--- advent-of-code/2024/day1/day1_test.go
+++ advent-of-code/2024/day1/day1_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-func TestDay1(t *testing.T) {
+func TestPart1(t *testing.T) {
 	file, err := os.Open("input")
 	if err != nil {
 		t.Fatal("can't open input file")
@@ -60,4 +60,52 @@ func TestDay1(t *testing.T) {
 	}
 
 	fmt.Println(sum)
+}
+
+func TestPart2(t *testing.T) {
+	file, err := os.Open("input")
+	if err != nil {
+		t.Fatal("can't open input file")
+	}
+	defer file.Close()
+
+	var column1 []int
+	var column2 []int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// ex: 31594   93577
+		line := scanner.Text()
+
+		lineStrSlice := strings.Split(line, "   ")
+		lineIntVar1, err := strconv.Atoi(lineStrSlice[0])
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		lineIntVar2, err := strconv.Atoi(lineStrSlice[1])
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		column1 = append(column1, lineIntVar1)
+		column2 = append(column2, lineIntVar2)
+	}
+
+	if scanner.Err() != nil {
+		t.Fatal("scanner err")
+	}
+
+	var score int
+	for _, v1 := range column1 {
+		var sum int
+		for _, v2 := range column2 {
+			if v1 == v2 {
+				sum += v1
+			}
+		}
+		score += sum
+	}
+
+	fmt.Println(score)
 }
