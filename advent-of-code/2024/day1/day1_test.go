1b8be56a6d6c72d87da0e14984959048d2a7ef65
sulinehk <sulinehk@gmail.com>
2025-12-28 14:33:44 +0800

    add advent-of-code day1

---
+++ advent-of-code/2024/day1/day1_test.go
@@ -0,0 +1,63 @@
+package day1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestDay1(t *testing.T) {
+	file, err := os.Open("input")
+	if err != nil {
+		t.Fatal("can't open input file")
+	}
+	defer file.Close()
+
+	var column1 []int
+	var column2 []int
+
+	// 1. scan data
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// ex: 31594   93577
+		line := scanner.Text()
+
+		lineStrSlice := strings.Split(line, "   ")
+		lineIntVar1, err := strconv.Atoi(lineStrSlice[0])
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		lineIntVar2, err := strconv.Atoi(lineStrSlice[1])
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		column1 = append(column1, lineIntVar1)
+		column2 = append(column2, lineIntVar2)
+	}
+
+	if scanner.Err() != nil {
+		t.Fatal("scanner err")
+	}
+
+	// 2. sort column1 and column2
+	sort.Ints(column1)
+	sort.Ints(column2)
+
+	// 3. get abs value and sum
+	var sum int
+	for i := range column1 {
+		distance := column1[i] - column2[i]
+		if distance < 0 {
+			distance = -distance
+		}
+		sum += distance
+	}
+
+	fmt.Println(sum)
+}
